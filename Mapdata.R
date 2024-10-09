0cce4e593043cdc96ce0616a1f19fc4ce06f7969
Kiara Thomas <kiarathomas@kiaras-imac.mynetworksettings.com>
2024-10-09 17:04:43 -0400

    Coding for mapping

--- Mapdata.R
+++ Mapdata.R
@@ -4,7 +4,6 @@ library(leafpop)
 library(dplyr)
 library(ggplot2)
 library(patchwork)
-library(htmltools)
 #location of coops #### 
 coops_maps <- st_intersection(census_2020_shp, coops_shp)  
 
@@ -12,17 +11,27 @@ coops_maps <- st_intersection(census_2020_shp, coops_shp)
 tract70data <- population_data(census_2020_shp, tract_70)
 tract80data <- population_data(census_2020_shp, tract_80)
 tract90data <- population_data(census_2020_shp, tract_90)
+tract2000data <- population_data(census_2020_shp, tract_2000)
+tract2010data <- population_data(census_2020_shp, tract_2010)
+tract2020data <- population_data(census_2020_shp, tract_2020)
 
 
-# Using source code plot_map to create tract80 pop map on mapview #### 
-mapview1970pop <- plot_map(coops_maps, tract70data)
-mapview1980pop <- plot_map(coops_maps, tract80data)
-mapview1990pop <- plot_map(coops_maps, tract90data)
+# Using source code plot_map to create pop map on mapview #### 
+mapview1970pop <- plot_pop_map(coops_maps, tract70data)
+mapview1980pop <- plot_pop_map(coops_maps, tract80data)
+mapview1990pop <- plot_pop_map(coops_maps, tract90data)
+
+
+# Using source code plot_map to create blk map on mapview #### 
+
+mapview1970blk <- plot_blk_mapview(coops_maps, tract70data)
+mapview1980blk <- plot_blk_map(coops_maps, tract80data)
+mapview1990blk <- plot_blk_map(coops_maps, tract90data)
 
 # Using source code plot_population_map to create tract pop maps on ggplot2 #### 
 
 ggplot1970pop <- plot_population_map(census_data = census_2020_shp, 
-                                      population_data = tract70bdata, 
+                                      population_data = tract70data, 
                                       tract_data = coops_maps) 
 ggplot1980pop <- plot_population_map(census_data = census_2020_shp, 
                                         population_data = tract80data,
@@ -30,8 +39,31 @@ ggplot1980pop <- plot_population_map(census_data = census_2020_shp,
 ggplot1990pop <- plot_population_map(census_data = census_2020_shp, 
                                      population_data = tract90data,
                                      tract_data = coops_maps)  
+ggplot2000pop <- plot_population_map(census_data = census_2020_shp, 
+                                     population_data = tract2000data, 
+                                     tract_data = coops_maps) 
+ggplot2010pop <- plot_population_map(census_data = census_2020_shp, 
+                                     population_data = tract2010data,
+                                     tract_data = coops_maps)  
+ggplot2020pop <- plot_population_map(census_data = census_2020_shp, 
+                                     population_data = tract2020data,
+                                     tract_data = coops_maps)  
 
-
+ 
 #Combining the pop maps #### 
-pop_map_combined <- ggplot1970pop + ggplot1980pop + ggplot1990pop
+pop_map_combined <- ggplot1970pop + ggplot1980pop 
 
+pop_map <- ggplot1970pop + ggplot2020pop
+
+
+# Using source code for black percent on ggplot ####
+ggplot1970blk <- plot_blk_map(census_2020_shp,tract70data, coops_maps)
+ggplot1980blk <- plot_blk_map(census_2020_shp,tract80data,coops_maps)
+ggplot1990blk <- plot_blk_map(census_2020_shp,tract90data,coops_maps)
+ggplot2000blk <- plot_blk_map(census_2020_shp,tract2000data,coops_maps)
+ggplot2010blk <- plot_blk_map(census_2020_shp,tract2010data,coops_maps)
+ggplot2020blk <- plot_blk_map(census_2020_shp,tract2020data,coops_maps)
+
+
+
+blk_map_combined <- ggplot1970blk + ggplot1990blk
--- MappingSourceCode.R
+++ MappingSourceCode.R
@@ -8,7 +8,7 @@ plot_population_map <- function(census_data, population_data, tract_data, title 
     geom_sf(data = population_data, aes(fill = pop)) +  
     
     # Use a viridis color scale for the population data, setting limits
-    scale_fill_viridis_c(option = "C", limits = c(0, 10000), 
+    scale_fill_viridis_c(option = "C", limits = c(0, 20000), 
                          direction = -1,
                          name = "Population") +  
     
@@ -25,6 +25,34 @@ plot_population_map <- function(census_data, population_data, tract_data, title 
     theme_minimal()
 }
 
+plot_blk_map <- function(census_data, population_data, tract_data, title = "Black Race By Region") {
+  
+  ggplot() + 
+    # First layer: Plot the census tract shapefile with borders only (no fill)
+    geom_sf(data = census_data, fill = NA, color = "black", size = 0.3) +
+    
+    # Second layer: Plot the population data
+    geom_sf(data = population_data, aes(fill = pblk)) +  
+    
+    # Use a viridis color scale for the population data, setting limits
+    scale_fill_viridis_c(option = "C", limits = c(0, 100), 
+                         direction = -1,
+                         name = "Percent Black") +  
+    
+    # Minimalistic theme for the plot
+    theme_minimal() +
+    
+    # Add labels for the title and legend
+    labs(title = title) +
+    
+    # Third layer: Add tract boundaries (if needed)
+    geom_sf(data = tract_data, color = "white", size = 0.5) +  # White borders
+    
+    # Apply minimal theme again
+    theme_minimal()
+}
+
+
 
 population_data <- function(census_data, population_data) {
   left_join(census_data, population_data)
@@ -34,7 +62,7 @@ population_data <- function(census_data, population_data) {
 
 library(mapview)
 
-plot_map <- function(data, popdata) { 
+plot_pop_map <- function(data, popdata) { 
   # Create the map for coops
   coops_map <- mapview(data, 
                        legend = TRUE, 
@@ -47,8 +75,31 @@ plot_map <- function(data, popdata) {
                        zcol = "pop", 
                        layer.name = "Population",
                        popup = popupTable(popdata, zcol = c("pop", "TRTID10")),
-                       zlim = c(0, 10000),  # Set limits for the color scale
+                       zlim = c(0, 20000),  # Set limits for the color scale
                        legend.title = "Population")  # Optional: Set legend title
+  
+  # Combine both maps into one view
+  combined_map <- coops_map + tract_map
+  
+  return(combined_map)  # Return the combined map
+}
+
+
+plot_blk_mapview <- function(data, racedata) { 
+  # Create the map for coops
+  coops_map <- mapview(data, 
+                       legend = TRUE, 
+                       layer.name = "Coops",
+                       popup = popupTable(data, zcol = c("Name")))
+  
+  # Create the map for tract data
+  tract_map <- mapview(racedata, 
+                       legend = TRUE, 
+                       zcol = "pop", 
+                       layer.name = "Percent by Black",
+                       popup = popupTable(racedata, zcol = c("pblk", "TRTID10")),
+                       zlim = c(0, 100),  # Set limits for the color scale
+                       legend.title = "Percent by Black")  # Optional: Set legend title
   
   # Combine both maps into one view
   combined_map <- coops_map + tract_map
