2401055ac63705bde73793771f3d6771d3705a0d
Kiara Thomas <kiarathomas@kiaras-imac.mynetworksettings.com>
2025-01-28 18:44:48 -0500

    Gentrification

--- testscriptgentri.R
+++ testscriptgentri.R
@@ -1,58 +1,92 @@
 
-n <- 30 
-random_sample_1970 <- tract_70[sample(nrow(tract_70), n), ]
+threshold70 <- quantile(tract_70$hinc, 0.40, na.rm = TRUE)
+threshold70
 
-median_value_1970 <- median(random_sample_1970$hinc, na.rm = TRUE)
+threshold80 <- quantile(tract_80$hinc, 0.40, na.rm = TRUE)
+threshold80
 
-threshold <- quantile(random_sample_1970$hinc, 0.40, na.rm = TRUE)
 
-fourt <- random_sample_1970[random_sample_1970$hinc <= threshold, ]
-fourt
-max(fourt$hinc)
-random_sample_1970$gent <- ifelse(random_sample_1970$hinc <= 8507.94, "Gentrifiable", "Non-Gentrifiable")
+threshold90 <- quantile(tract_90$hinc, 0.40, na.rm = TRUE)
+threshold90
 
+
+threshold00 <- quantile(tract_2000$hinc, 0.40, na.rm = TRUE)
+threshold00
+
+
+threshold10 <- quantile(tract_2010$hinc, 0.40, na.rm = TRUE)
+threshold10
+
+
+threshold20 <- quantile(tract_2020$hinc, 0.40, na.rm = TRUE)
+threshold20
 
 library(dplyr)
-sample_1970_1980 <- left_join(random_sample_1970, tract_80, by = "TRTID10")
-
-sample_1970_1980$college_rate <- sample_1970_1980$pcol.y - sample_1970_1980$pcol.x
-
-tres <- quantile(sample_1970_1980$college_rate, 0.50, na.rm = TRUE) 
-col <- sample_1970_1980[sample_1970_1980$college_rate >= tres, ]
-col
-sample_1970_1980$gent2 <- ifelse(sample_1970_1980$college_rate <= 0.02, "Lower", "Higher") 
-
-need <- sample_1970_1980 %>% select(gent2, gent, college_rate)
-
-need <- need %>% mutate(gentri = ifelse(gent2 == "Higher" & gent == "Gentrifiable", "Gentrified", "Not Gentrified"))
-
-
+tract <- tract %>%
+mutate(
+  gent = case_when(
+    year >= 1970 & year < 1980 ~ hinc <= 7906.25,  # 1970s threshold
+    year >= 1980 & year < 1990 ~ hinc <= 12672,    # 1980s threshold
+    year >= 1990 & year < 2000 ~ hinc <= 27075.6,    # 1990s threshold
+    year >= 2000 & year < 2010 ~ hinc <= 34212.94, 
+    year >= 2010 & year < 2020 ~ hinc <= 46853.4,
+    year >= 2020 & year < 2030 ~ hinc <= 59203.2,
+    TRUE ~ FALSE  # Default case, for years outside defined decades
+  ) 
+)
 
 
-
+tract <- tract %>% arrange(TRTID10, year)
 
 
 
-# Full sample ####
+tract <- tract %>% group_by(TRTID10) %>% mutate(col_per = pcol - lag(pcol))
 
-thresholda <- quantile(tract_70$hinc, 0.40, na.rm = TRUE) 
-tract_70$gent <- ifelse(tract_70$hinc <= 7906.25, "Gentrifiable", "Non-Gentrifiable")
-
-tract$gent <- ifelse(tract$year == 1970 & tract$hinc <= 7906.25, "Gentrifiable", "Non-Gentrifiable")
-tract_70$gent70 <- ifelse(tract_70$hinc <= 7906.25, "Gentrifiable", "Non-Gentrifiable")
-
-college_rates <- left_join(tract_70,tract_80, by = "TRTID10") %>% rename(col1970 = pcol.x, col1980 = pcol.y, hinc1970 = hinc.x, hinc1980 = hinc.y) 
-college_rates$gent70 <- ifelse(college_rates$hinc1970 <= 7906.25, "Gentrifiable", "Non-Gentrifiable")
-college_rates$change7080 <- college_rates$col1980 - college_rates$col1970
-college_rates <- college_rates %>% select(col1970,hinc1970, gent70, col1980, hinc1980, gent80) 
-tresb <- quantile(college_rates$change7080, 0.50, na.rm = TRUE) 
-college_rates$gent1980 <- ifelse(college_rates$change >= 0.02 & college_rates$gent == "Gentrifiable", "Gentrified", "Not Gentrified")
+tract <- tract %>% arrange(year)
 
 
+tract <- tract %>%
+  mutate(
+    colr = case_when(
+      year >= 1980 & year < 1990 ~ col_per >= 0.02,    # 1980s threshold
+      year >= 1990 & year < 2000 ~ col_per >= 0.03 ,    # 1990s threshold
+      year >= 2000 & year < 2010 ~ col_per >= 0.02, 
+      year >= 2010 & year < 2020 ~ col_per >= 0.04,
+      year >= 2020 & year < 2030 ~ col_per >= 0.02,
+      TRUE ~ FALSE  # Default case, for years outside defined decades
+    ) 
+  )
 
 
-thresholdb <- quantile(college_rates$hinc1980, 0.40, na.rm = TRUE)
 
-college_rates$gent80 <- ifelse(college_rates$hinc1980 <= 12662.4, "Gentrifiable", "Non-Gentrifiable") 
 
+
+
+
+colper80fr <- tract %>% filter(year == 1980)
+quantile(colper80fr$col_per, 0.50, na.rm = TRUE)
+
+
+colper90fr <- tract %>% filter(year == 1990)
+quantile(colper90fr$col_per, 0.50, na.rm = TRUE)
+
+
+colper00fr <- tract %>% filter(year == 2000)
+quantile(colper00fr$col_per, 0.50, na.rm = TRUE)
+
+
+colper10fr <- tract %>% filter(year == 2010)
+quantile(colper10fr$col_per, 0.50, na.rm = TRUE)
+
+
+colper20fr <- tract %>% filter(year == 2020)
+quantile(colper20fr$col_per, 0.50, na.rm = TRUE)
+
+
+tract <- tract %>%
+  mutate(gentstat = ifelse(
+gent == TRUE & !is.na(colr) & colr == TRUE,  
+    "gentrified",                                                 
+    "not-gentrified"                                              
+  ))
 
