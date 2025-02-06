e3a8a97551a98705212b675b853022054c916f08
hjacobcarlson <h.jacob.carlson@gmail.com>
2025-02-06 12:17:10 -0500

    Create new plot_map function to create maps for any variable

--- MappingSourceCode.R
+++ MappingSourceCode.R
@@ -1,3 +1,47 @@
+
+
+# ggplot2 maps ####
+
+plot_map <- function(geodata, mapvar, addcoops = TRUE, 
+                     title = "") {
+  
+  mapvar <- enquo(mapvar)
+  
+  p <- ggplot() +  
+    
+    # Second layer: Plot the population data
+    geom_sf(data = geodata, aes(fill = !!mapvar)) +  
+    
+    # Use a viridis color scale for the population data, setting limits
+    scale_fill_viridis_c(option = "C", limits = c(0, 10000),
+                         direction = -1,
+                         name = title) +
+    
+    # Minimalistic theme for the plot
+    theme_minimal() +
+    
+    # Add labels for the title and legend
+    labs(title = title) +
+    
+    theme(axis.text.x = element_blank(), axis.ticks.x = element_blank(),
+          axis.text.y = element_blank(), axis.ticks.y = element_blank(),
+          axis.line = element_blank(),
+          panel.grid.major = element_blank(), panel.grid.minor = element_blank())
+  
+  if(addcoops == TRUE) {
+    p <- p +
+      geom_sf(data = coops_shp, color = "black", size = 0.5, na.rm = TRUE)
+    
+    p
+    
+  } else {
+    p
+    
+  }
+}
+
+
+
 # Population map ####
 
 plot_population_map <- function(population_data, coops, title = "Population by Region") {
