b3af54671f6861691f4a92e27388c2e77caa2055
hjacobcarlson <h.jacob.carlson@gmail.com>
2025-02-06 12:28:23 -0500

    Finish recreating maps with new plot_map() function

--- Mapdata.R
+++ Mapdata.R
@@ -72,63 +72,61 @@ mapview2010blk <- plot_blk_mapview(coops_maps, tract2010shp)
 mapview2020blk <- plot_blk_mapview(coops_maps, tract2020shp)
 
 
-# Using source code plot_population_map to create tract pop maps on ggplot2 #### 
+# Create maps by year, variable ####
 
-ggplot1970pop <- plot_map( tract70shp, coops_maps)
-ggplot1980pop <- plot_population_map(census_data = census_2010_shp, 
-                                        population_data = tract80shp,
-                                        tract_data = coops_maps)  
-ggplot1990pop <- plot_population_map(census_data = census_2010_shp, 
-                                     population_data = tract90shp,
-                                     tract_data = coops_maps)  
-ggplot2000pop <- plot_population_map(census_data = census_2010_shp, 
-                                     population_data = tract2000shp, 
-                                     tract_data = coops_maps) 
-ggplot2010pop <- plot_population_map(census_data = census_2010_shp, 
-                                     population_data = tract2010shp,
-                                     tract_data = coops_maps)  
-ggplot2020pop <- plot_population_map(census_data = census_2010_shp, 
-                                     population_data = tract2020shp,
-                                     tract_data = coops_maps)  
+## Total Population ####
+
+ggplot1970pop <- plot_map(tract70shp, pop, title = "Population")
+ggplot1980pop <- plot_map(tract80shp, pop, title = "Population")
+ggplot1990pop <- plot_map(tract90shp, pop, title = "Population")
+ggplot2000pop <- plot_map(tract2000shp, pop, title = "Population")
+ggplot2010pop <- plot_map(tract2010shp, pop, title = "Population")
+ggplot2020pop <- plot_map(tract2020shp, pop, title = "Population")
+
+
+## Percent Black ####
+
+ggplot1970pblk <- plot_map(tract70shp, pblk, title = "Pct. Black")
+ggplot1980pblk <- plot_map(tract80shp, pblk, title = "Pct. Black")
+ggplot1990pblk <- plot_map(tract90shp, pblk, title = "Pct. Black")
+ggplot2000pblk <- plot_map(tract2000shp, pblk, title = "Pct. Black")
+ggplot2010pblk <- plot_map(tract2010shp, pblk, title = "Pct. Black")
+ggplot2020pblk <- plot_map(tract2020shp, pblk, title = "Pct. Black")
+
+
+## Percent white ####
+
+ggplot1970pwht <- plot_map(tract70shp, pwht, title = "Pct. White")
+ggplot1980pwht <- plot_map(tract80shp, pwht, title = "Pct. White")
+ggplot1990pwht <- plot_map(tract90shp, pwht, title = "Pct. White")
+ggplot2000pwht <- plot_map(tract2000shp, pwht, title = "Pct. White")
+ggplot2010pwht <- plot_map(tract2010shp, pwht, title = "Pct. White")
+ggplot2020pwht <- plot_map(tract2020shp, pwht, title = "Pct. White")
+
+
+## Percent Unemployed
+
+ggplot1970punemp <- plot_map(tract70shp, punemp, title = "Pct. Unemployed")
+ggplot1980punemp <- plot_map(tract80shp, punemp, title = "Pct. Unemployed")
+ggplot1990punemp <- plot_map(tract90shp, punemp, title = "Pct. Unemployed")
+ggplot2000punemp <- plot_map(tract2000shp, punemp, title = "Pct. Unemployed")
+ggplot2010punemp <- plot_map(tract2010shp, punemp, title = "Pct. Unemployed")
+ggplot2020punemp <- plot_map(tract2020shp, punemp, title = "Pct. Unemployed")
+
+
+## Percent college graduates ####
+
+
+
+
+# NOTE: Below here, kept (temporarily?) from previous version
+# Using source code for 
+library(gridExtra)
+grid.arrange(ggplot1970blk, ggplot1980blk, ncol = 2)
+
+
 
 #Combining the pop maps #### 
 pop_map_combined <- ggplot1970pop + ggplot2020pop 
 
 pop_map <- ggplot1970pop + ggplot2020pop
-
-
-
-# Using source code for black percent on ggplot ####
-ggplot1970blk <- plot_race_map(census_2010_shp, tract70shp, coops_maps, "pblk")
-ggplot1980blk <- plot_race_map(census_2010_shp, tract80shp, coops_maps, "pblk")
-ggplot1990blk <- plot_race_map(census_2010_shp, tract90shp, coops_maps, "pblk")
-ggplot2000blk <- plot_race_map(census_2010_shp, tract2000shp, coops_maps, "pblk")
-ggplot2010blk <- plot_race_map(census_2010_shp, tract2010shp, coops_maps, "pblk")
-ggplot2020blk <- plot_race_map(census_2010_shp, tract2020shp, coops_maps, "pblk")
-
-
-# Using source code for white percent on ggplot ####
-
-ggplot1970wht <- plot_race_map(census_2010_shp, tract70shp, coops_maps, "pwht")
-ggplot1980wht <- plot_race_map(census_2010_shp, tract80shp, coops_maps, "pwht")
-ggplot1990wht <- plot_race_map(census_2010_shp, tract90shp, coops_maps, "pwht")
-ggplot2000wht <- plot_race_map(census_2010_shp, tract2000shp, coops_maps, "pwht")
-ggplot2010wht <- plot_race_map(census_2010_shp, tract2010shp, coops_maps, "pwht")
-ggplot2020wht <- plot_race_map(census_2010_shp, tract2020shp, coops_maps, "pwht")
-
-
-# Using source code for unemployment percent ####
-ggplot1970unemp <- plot_unemp(census_2010_shp, tract70shp, coops_maps, "punemp")
-ggplot1980unemp <- plot_unemp(census_2010_shp, tract80shp, coops_maps, "punemp")
-ggplot1990unemp <- plot_unemp(census_2010_shp, tract90shp, coops_maps, "punemp")
-ggplot2000unemp <- plot_unemp(census_2010_shp, tract2000shp, coops_maps, "punemp")
-ggplot2010unemp <- plot_unemp(census_2010_shp, tract2010shp, coops_maps, "punemp")
-ggplot2020unemp <- plot_unemp(census_2010_shp, tract2020shp, coops_maps, "punemp")
-
-
-# Using source code for college graduates percent ####
-
-# Using source code for 
-library(gridExtra)
-grid.arrange(ggplot1970blk, ggplot1980blk, ncol = 2)
-
