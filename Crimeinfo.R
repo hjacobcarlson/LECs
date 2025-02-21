2ec8ce54fc098c65e2a4d823eb3dda848fb058f0
Kiara Thomas <kiarathomas@mac.mynetworksettings.com>
2025-02-20 19:06:03 -0500

    New Crime Info for 1990 Added

--- Crimeinfo.R
+++ Crimeinfo.R
@@ -1,18 +1,16 @@
-crime_data <- read.csv("~/Downloads/Book 15(Sheet1) (2).csv")
-
 library(tidyr)
-
-colnames(crime_data) <- gsub("^X", "", colnames(crime_data))
-
-crime_long <- crime_data %>%
-  pivot_longer(
-    cols = starts_with("20"), # Select year columns
-    names_to = "Year", # New column for year
-    values_to = "Count")
-summary(crime_long)
-
-
 library(dplyr)
-crime_long <- crime_long %>% arrange(Year, CRIME)
+
+crime_data <- read.csv("data/crime_original.csv") # read in data
+
+colnames(crime_data) <- gsub("^X", "", colnames(crime_data)) #remove "X" in front of years
+
+
+crime_data <- crime_data %>% 
+  pivot_longer(
+    cols = matches("^20|^1990"), # Select year columns
+    names_to = "Year", # New column for year
+    values_to = "Count") %>% arrange(Year, CRIME)  
+
 
 
