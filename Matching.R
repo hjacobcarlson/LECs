0ca9f2c351a752ee73c8a05f9735086b43d78fe4
Kiara Thomas <kt3143@gmail.com>
2025-04-29 21:23:21 -0400

    New matching coding. In this project is cleaning the addresses and joining to rent data

--- Matching.R
+++ Matching.R
@@ -2,6 +2,34 @@
 library(tidyverse)
 library(dplyr)
 library(stringr)
+library(tidyverse)
+
+boroughs <- c("Manhattan", "New York", "Brooklyn", "Queens", "Bronx")
+
+coops_test <- read_csv("data/NY Co-op Data(Sheet1).csv") %>% 
+  filter(!is.na(ID)) %>% 
+  filter(!is.na(LONG)) %>% 
+  filter(!Type %in% c("Affordable, Former HUD 213", "Senior", "Student co-op")) # omitting some types of "coops", may change later 
+
+coops_test$borough <- str_extract(coops_test$ADDRESS, paste(boroughs, collapse = "|")) 
+
+coops_test$street_address <- str_replace(coops_test$ADDRESS, paste(boroughs, collapse = "|"), "") 
+coops_test$street_address <- str_trim(coops_test$street_address) # Remove any extra spaces 
+
+coops_cleaned <- coops_test %>% 
+  mutate(
+    zip_code = str_extract(street_address, "\\d{5}"),  # Correct escape for digits
+    street_address = str_remove(street_address, "NY\\s*\\d{5}"),  # Remove NY and zip (escaped correctly)
+    street_address = str_trim(str_remove(street_address, "\\d{5}$")),  # Just in case ZIP is still stuck at the end
+    street_address = street_address %>%
+      str_replace_all(",", "") %>%
+      str_replace_all("The ", "") %>%
+      str_replace_all("New York", "") %>%
+      str_replace_all("NY", "")
+  )
+
+coops_cleaned$street_address <- toupper(coops_cleaned$street_address) 
+
 # import borough data 
 brooklyn_rent <- read_csv("data/b3_brooklyn_coop_comp_032224.csv")
 brooklyn_address <- select(brooklyn_rent, `Address...2`, "Neighborhood...3", "Year Built...6")
@@ -14,8 +42,10 @@ manhattan_address <- select(manhattan_rent, `Address...2`, "Neighborhood...3")
 ny_address <- bind_rows(bronx_address, manhattan_address, brooklyn_address)
 ny_address <- ny_address %>% mutate(Address = str_squish(Address))
 
+coops_clean_test <- coops_cleaned %>%
+  rename(Address = street_address) %>%
+  select(Address, Type) 
 
-coops_clean_test <- coops_cleaned %>% select("Address", "Type")
 coops_clean_test <- coops_clean_test %>%
   mutate(Address = str_trim(Address, side = "right")) 
 
