617890ed0620c47f5bf4f5ca437a6b72a0e1fb03
Kiara Thomas <kt3143@gmail.com>
2025-07-10 20:44:02 -0400

    your commit message

--- Matching.R
+++ Matching.R
@@ -33,6 +33,10 @@ coops_cleaned$street_address <- toupper(coops_cleaned$street_address)
 # import borough data 
 brooklyn_rent <- read_csv("data/b3_brooklyn_coop_comp_032224.csv")
 brooklyn_address <- select(brooklyn_rent, `Address...2`, "Neighborhood...3", "Year Built...6")
+brooklyn_rent <- brooklyn_rent %>% 
+                  rename(Address = Address...2)
+
+testtest <- left_join(coops_cleaned, brooklyn_rent)
 
 bronx_rent <- read_csv("data/b2_bronx_coop_comp_d030122 (1).csv") 
 bronx_address <- select(bronx_rent, `Address...2`, "Neighborhood...3")
@@ -66,4 +70,35 @@ coops_rent <- left_join(coops_clean_test, ny_address)
 write.csv(coops_rent, "coopsrent.csv", row.names = FALSE) 
 
 
+mh_test_23 <- read_csv("~/Downloads/data/b1_manhattan_coop_comp_d032223.csv")
+mh_test_22 <- read_csv("~/Downloads/data/b1_manhattan_coop_comp_d030122 (2).csv")
+mh_test_13 <- read_csv("~/Downloads/Sheet1-Table 1.csv")
 
+# Assume df is your data frame and row 1 contains column names
+names(mh_test_23) <- as.character(mh_test_23[1, ])  # Set first row as column names
+mh_test_23 <- mh_test_23[-1, ]  # Remove the first row
+
+mh_test_22 <- mh_test_22 %>%
+  rename(Address = Address...2) %>% select(Address) 
+
+mh_test13 <- mh_test_13 %>%
+  rename(Address = Address...2) %>% select(Address) 
+
+names(mh_test_23) <- make.names(names(mh_test_23), unique = TRUE)
+
+mh_test23 <- mh_test_23 %>% select(Address)
+
+df_unique <- df %>% distinct(Address, .keep_all = TRUE)
+mh_test23 <- mh_test23 %>% distinct(Address, .keep_all = TRUE)
+mh_test22 <- mh_test_22 %>% distinct(Address, .keep_all = TRUE)
+mh_test13 <- mh_test13 %>% distinct(Address, .keep_all = TRUE)
+
+mh_test13 <- mh_test13 %>% mutate(Address = str_squish(Address))
+mh_test_23b <- mh_test23 %>% mutate(Address = str_squish(Address))
+
+joined_mh <- full_join(mh_test23, mh_test22, by = "Address")
+
+inner_joined_mh <- inner_join(mh_test23,mh_test22, by = "Address")
+
+joined_mh_2313 <- full_join(mh_test23, mh_test13, by = "Address")
+inner_joined_mh2313 <- inner_join(mh_test23,mh_test13, by = "Address")
