ae9adc90dc8e186e08235fc990091ac2e0191015
Kiara Thomas <kt3143@gmail.com>
2025-07-26 20:02:02 -0400

    new coding for descriptives

--- descriptives2.R
+++ descriptives2.R
@@ -1,24 +1,44 @@
-# creating a dataset with both matched and unmatched coops with coop demographics #### 
-coopsrnt_dem <- bind_rows(matched_2020,unmatched_2020)
+# remove geometry to make descriptions look cleaner 
+coops_desc <- coops_map_dem %>%
+  st_drop_geometry()
 
-
-
-coops <-coopsrnt_dem %>%
-  mutate(matched = !is.na(Neighborhood))
-
-
-
-p <- coops %>%
+# graphs for pblk 
+his_blk <- coops_desc %>%
   ggplot(aes(x = pblk)) +
   geom_histogram() +
   facet_wrap(~matched)
-p
 
-desc <- coops %>%
+box_blk <-  coops_desc %>%
+  ggplot(aes(x = pblk)) +
+  geom_boxplot() +
+  facet_wrap(~matched)
+
+density_blk <- coops_desc %>%
+  ggplot(aes(x = pblk)) +
+  geom_density() +
+  facet_wrap(~matched)
+# graphs for pwht 
+his_wht <- coops_desc %>%
+  ggplot(aes(x = pwht)) +
+  geom_histogram() +
+  facet_wrap(~matched)
+
+box_wht <-  coops_desc %>%
+  ggplot(aes(x = pwht)) +
+  geom_boxplot() +
+  facet_wrap(~matched)
+
+density_wht <- coops_desc %>%
+  ggplot(aes(x = pwht)) +
+  geom_density() +
+  facet_wrap(~matched)
+
+# descriptives 
+desc <- coops_desc %>%
   group_by(matched) %>%
   summarize(mhinc = mean(hinc, na.rm = TRUE),
             sdhinc = sd(hinc, na.rm = TRUE),
             mpblk = mean(pblk, na.rm = TRUE),
             sdpblk = sd(pblk, na.rm = TRUE))
 
-desc
+desc 
--- mappingunmvsm.R
+++ mappingunmvsm.R
@@ -4,7 +4,7 @@ library(dplyr)
 library(stringr)
 library(tidyverse)
 library(ggplot2)
-
+library(sf)
 # creating new dataframes with the two categories of unmatched coops and matched coops ####
 matched_coops <- coops_rent %>%
   filter(!is.na(Neighborhood)) 
@@ -189,3 +189,78 @@ ggplot() +
   labs(title = "Percent Unemployed in Unmatched Coops",
        fill = "Percent Unemployed",
        color = "Point Group") 
+
+
+
+
+
+
+# NEW TYPE of MAPS #### 
+coops_map <- coops_maps %>% select(geometry,ID)
+coops_map_dem <- left_join(coops,coops_map, by = "ID") # dataframe that contains demographics and geometry of the POINTS 
+census <- left_join(census_2010_shp,coops, by = "TRTID10") # dataframe that contains demographics and geometry of the census tracts 
+
+census <- st_as_sf(census)
+coops_map_dem <- st_as_sf(coops_map_dem)
+
+# general 
+
+ggplot() +
+  geom_sf(data = census, fill = "gray90", color = "white", size = 0.2) +  # base NYC map
+  geom_sf(data = coops_map_dem, aes(color = matched), size = 2, alpha = 0.5) +             # your points
+  theme_minimal() + 
+  labs(title = "Map of Coops")
+
+
+# unemployed 
+ggplot() +
+  # Base map: polygons filled by your external variable
+  geom_sf(data = census, aes(fill = punemp), color = "white", size = 0.2) +
+  
+  # Points: color based on `matched` column
+  geom_sf(data = coops_map_dem, aes(color = matched), size = 2, alpha = 0.1) +
+  
+  # Styling
+  scale_fill_gradient(low = "lightblue", high = "darkblue", na.value = "gray90") +
+scale_color_manual(values = c("FALSE" = "black", "TRUE" = "red")) +  # Customize colors
+  theme_minimal() +
+  labs(title = "Percent Unemployed in Coops",
+       fill = "Percent Unemployed",
+       color = "Matched Status")
+
+# pblk
+ggplot() +
+  # Base map: polygons filled by your external variable
+  geom_sf(data = census, aes(fill = pblk), color = "white", size = 0.2) +
+  
+  # Points: color based on `matched` column
+  geom_sf(data = coops_map_dem, aes(color = matched), size = 2, alpha = 0.1) +
+  
+  # Styling
+  scale_fill_gradient(low = "lightblue", high = "darkblue", na.value = "gray90") +
+  scale_color_manual(values = c("FALSE" = "black", "TRUE" = "red")) +  # Customize colors
+  theme_minimal() +
+  labs(title = "Percent Black in Coops",
+       fill = "Percent Black",
+       color = "Matched Status")
+
+# pwht with circles and triangles 
+ggplot() +
+  # Base map: NY shapefile with light-to-dark blue fill
+  geom_sf(data = census, aes(fill = pwht), color = "white", size = 0.2) +
+  
+  # Points: shape based on matched
+  geom_sf(data = coops_map_dem, aes(shape = matched), size = 2, color = "red", alpha = 0.1) +
+  
+  # Fill scale for background
+  scale_fill_gradient(low = "lightblue", high = "darkblue", na.value = "gray90") +
+  
+  # Shape mapping: circle (16) for TRUE, triangle (17) for FALSE
+  scale_shape_manual(values = c("TRUE" = 16, "FALSE" = 17)) +
+  
+  theme_minimal() +
+  labs(
+    title = "Percent White in Coops",
+    fill = "Percent White",
+    shape = "Matched Status"
+  )
